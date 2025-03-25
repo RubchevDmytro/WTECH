8b2f6493842743de41af855c9ac2ada9548727a8
Dima <dmitryr757@gmail.com>
2025-03-25 10:42:58 +0100

    Not full commit before

--- product_form_script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("add-image-btn").addEventListener("click", function() {
-    let uploadContainer = document.getElementById("upload-boxes");
-    let newUploadBox = document.createElement("div");
-    newUploadBox.classList.add("upload-box");
-    newUploadBox.innerHTML = "Upload File <br> (JPG, PNG, PDF)";
-    uploadContainer.appendChild(newUploadBox);
-});
-
