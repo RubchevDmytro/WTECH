8676d0cb603e34e5ccfaa5b18eac6997a3287bd8
Dima <dmitryr757@gmail.com>
2025-04-24 14:26:01 +0200

    some refactored

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,37 @@
 <?php
+ 
+ use App\Http\Controllers\AuthController;
+ use App\Http\Controllers\CategoryController;
+ use App\Http\Controllers\ProductController;
+ use App\Http\Controllers\CartController;
+ use App\Http\Controllers\OrderController;
+ use App\Http\Controllers\ProductImageController;
+ 
+ 
+ Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+ Route::post('/register', [AuthController::class, 'register'])->name('register');
+ 
+ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+ Route::post('/login', [AuthController::class, 'login'])->name('login');
+ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+ 
+ Route::get('/', [ProductController::class, 'index'])->name('main_page');
+ //temperary
+Route::get('/product/upload-image', [ProductImageController::class, 'showUploadForm'])->name('product.upload_image');
+Route::post('/product/upload-image', [ProductImageController::class, 'uploadImage'])->name('product.upload_image.store');
+ 
+Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
+ 
+Route::get('/order/confirm', [OrderController::class, 'confirm'])->name('order.confirm');
+Route::post('/order/create', [OrderController::class, 'create'])->name('order.create');
+ 
+Route::get('/autocomplete', [ProductController::class, 'autocomplete'])->name('autocomplete');
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+Route::patch('/cart/update/{cart}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('/cart/remove/{cart}', [CartController::class, 'remove'])->name('cart.remove');
 
+<<<<<<< HEAD
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
@@ -49,4 +81,17 @@ Route::middleware('auth')->group(function () {
         Route::get('/products', [ProductController::class, 'adminIndex'])->name('products.index');
     });
 
+=======
+
+Route::post('/order/create', [OrderController::class, 'create'])->name('order.create');
+
+
+ 
+Route::middleware('admin')->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+    Route::get('/admin/admin_menu', [AdminController::class, 'index'])->name('admin.menu');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class)->except(['index']);
+    Route::get('/products', [ProductController::class, 'adminIndex'])->name('products.index');
+>>>>>>> 88fcc9a (some refactored)
 });
